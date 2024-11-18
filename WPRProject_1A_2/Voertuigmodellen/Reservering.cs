95c42ccd85bc9b427ae7b1352d872a39cff74679
xande <23096780@student.hhs.nl>
2024-11-18 10:11:07 +0100

    Hij doet?

--- WPRProject_1A_2/Voertuigmodellen/Reservering.cs
+++ WPRProject_1A_2/Voertuigmodellen/Reservering.cs
@@ -2,5 +2,8 @@
 
 public class Reservering
 {
+    public int Id { get; set; }
     public required Voertuig Voertuig { get; set; }
+    public required DateTime Begindatum { get; set; }
+    public required DateTime Einddatum { get; set; }
 }
