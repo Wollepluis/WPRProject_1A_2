b508eb38c8cbe63f7eff481e7f036e671c1edd61
melle <Mellewessel@gmail.com>
2024-11-18 13:48:06 +0100

    Classes gemaakt van Abonnement, Bedrijf en Adres

---
+++ WPRProject_1A_2/Abonnement/Bedrijf.cs
@@ -0,0 +1,25 @@
+﻿namespace WPRProject_1A_2.Abonnement;
+
+public class Bedrijf
+{
+    public int Id { get; set; }
+    public required string Bederijfsnaam { get; set; }
+    public required string Domeinnaam { get; set; }
+    public required Adres Adres { get; set; }
+    public required int KVKNummer { get; set; }
+    public Abonnement? Abonnement { get; set; }
+    /*public required List<Account> Accounts { get; set; }
+
+    public Bedrijf(int id, string bederijfsnaam, string domeinnaam, Adres adres, int kVKNummer, Abonnement abonnement, List<Account> accounts)
+    {
+        Id = id;
+        Bederijfsnaam = bederijfsnaam;
+        Domeinnaam = domeinnaam;
+        Adres = adres;
+        KVKNummer = kVKNummer;
+        Abonnement = abonnement;
+        Accounts = accounts;
+    }
+    */
+    
+}
