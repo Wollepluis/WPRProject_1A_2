4ad99cfb3bc707297d723f5f8b16896daebd76c2
xande <23096780@student.hhs.nl>
2024-11-18 12:14:47 +0100

    Attributen en methodes van Reservering.cs en Voertuig.cs toegevoegd.

--- WPRProject_1A_2/Voertuigmodellen/Reservering.cs
+++ WPRProject_1A_2/Voertuigmodellen/Reservering.cs
@@ -1,9 +1,20 @@
-﻿namespace WPRProject_1A_2.Voertuigmodellen;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WPRProject_1A_2.Voertuigmodellen;
 
 public class Reservering
 {
+    [Key]
     public int Id { get; set; }
+    
+    public int VoertuigId { get; set; }
     public required Voertuig Voertuig { get; set; }
+    
     public required DateTime Begindatum { get; set; }
     public required DateTime Einddatum { get; set; }
+
+    public Reservering()
+    {
+        
+    }
 }
--- WPRProject_1A_2/Voertuigmodellen/Voertuig.cs
+++ WPRProject_1A_2/Voertuigmodellen/Voertuig.cs
@@ -1,6 +1,65 @@
-﻿namespace WPRProject_1A_2.Voertuigmodellen;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WPRProject_1A_2.Voertuigmodellen;
 
 public class Voertuig
 {
-    public List<Reservering>? Reserveringen { get; set; }
+    [Key]
+    public int Id { set; get; }
+
+
+    public enum TypeVoertuigEnum { Auto, Camper, Caravan }
+    public required TypeVoertuigEnum TypeVoertuig { set; get; }
+    
+    public enum VoertuigStatusEnum { Beschikbaar, Verhuurd, InReparatie, Geblokkeerd }
+    public required VoertuigStatusEnum VoertuigStatus { set; get; }
+    
+    private string kenteken;
+    public string Kenteken
+    {
+        set { kenteken = value; }
+        get { return kenteken; }
+    }
+    private List<Reservering> reserveringen { get; set; }
+    public List<Reservering> Reserveringen
+    {
+        set { reserveringen = value; }
+        get { return reserveringen; }
+    }
+    
+    public required string Merk { set; get; }
+    public required string Model { set; get; }
+    public required string Kleur { set; get; }
+    public required int Aanschafjaar { set; get; }
+
+    public void SetIsVerhuurd()
+    {
+        
+    }
+
+    public bool GetIsVerhuurd()
+    {
+        return false; //Nog aanpassen!!!!!!!!!!!!!!
+    }
+
+    public void ReserveringToevoegen(Reservering reservering)
+    {
+        Reserveringen.Add(reservering);
+    }
+
+    public void SchadeClaimToevoegen()
+    {
+        //Aanpassen
+    }
+
+    public List<Voertuig> VoertuigFilter()
+    {
+        List<Voertuig> voertuigen = new List<Voertuig>();
+        //
+        // Nog aanpassen!!!!!!!
+        //
+        return voertuigen;
+    }
+
+
 }
