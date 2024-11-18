e5b8fad1ff87ee102fa32dcd9d15f964c4e91ca8
Mark Wilbrink <23050063@student.hhs.nl>
2024-11-18 14:30:02 +0100

    Account classes aangemaakt met lege functies.

---
+++ WPRProject_1A_2/Accounts/Account_Zakelijk_Huurder.cs
@@ -0,0 +1,16 @@
+﻿using WPRProject_1A_2.Voertuigmodellen;
+
+namespace WPRProject_1A_2.Accounts;
+
+public class Account_Zakelijk_Huurder : Account_Zakelijk
+{
+    public Account_Zakelijk_Huurder(string email, string wachtwoord/*, Bedrijf bedrijf*/) : base(email, wachtwoord/*, bedrijf*/)
+    {
+        
+    }
+
+    public void HuurVoertuig(Voertuig voertuig)
+    {
+        
+    }
+}
