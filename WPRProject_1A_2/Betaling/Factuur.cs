638a07f4c7a8e8b15702ef5e12d6f88d4405c393
MorrisHHS <23085975@student.hhs.nl>
2024-11-22 12:23:11 +0100

    Factuur

---
+++ WPRProject_1A_2/Betaling/Factuur.cs
@@ -0,0 +1,33 @@
+﻿namespace WPRProject_1A_2.Betaling;
+
+public class Factuur
+{
+    int FactuurId { get; set; }
+    int Prijs { get; set; }
+    int Korting { get; set; }
+
+    public Factuur(int factuurId, int prijs, int korting)
+    {
+        FactuurId = factuurId;
+        Prijs = prijs;
+        Korting = korting;
+    }
+
+    public int StelFactuurOp()
+    {
+        int kortingsBedrag = (Prijs * Korting) / 100;
+        int totaalPrijs = Prijs - kortingsBedrag;
+        return totaalPrijs;
+    }
+
+    public override string ToString()
+    {
+        return $"Factuur ID: {FactuurId}\n" +
+               $"Prijs: €{Prijs}\n" +
+               $"Korting: {Korting}%\n" +
+               $"Totaalbedrag na korting: €{StelFactuurOp()}";
+    }
+}
+
+
+
