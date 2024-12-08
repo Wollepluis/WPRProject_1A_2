d1487844faffaf76a30a0e46a12aabc5844050ea
Wollepluis <mark2492@gmail.com>
2024-12-08 22:55:04 +0100

    Reparatie en Schadeclaim classes toegevoegd

---
+++ WPRRewrite/Modellen/Schadeclaim.cs
@@ -0,0 +1,16 @@
+﻿using WPRRewrite.Modellen.Voertuigen;
+
+namespace WPRRewrite.Modellen;
+
+public class Schadeclaim
+{
+    public int SchadeclaimId { get; set; }
+    public string Beschrijving { get; set; }
+    public DateTime Datum { get; set; }
+    public List<Reparatie> Reparaties { get; set; }
+
+    public void AddReparatie(Reparatie reparatie)
+    {
+        Reparaties.Add(reparatie);
+    }
+}
---
+++ WPRRewrite/Modellen/Voertuigen/Reparatie.cs
@@ -0,0 +1,9 @@
+﻿namespace WPRRewrite.Modellen.Voertuigen;
+
+public class Reparatie
+{
+    public int ReparatieId { get; set; }
+    public string Beschrijving { get; set; }
+    public DateTime ReparatieDatum { get; set; }
+    public List<String> Opmerkingen { get; set; }
+}
