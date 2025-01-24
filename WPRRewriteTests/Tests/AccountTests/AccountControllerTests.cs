2eaddaacd488867e9c785cf4d2a0b469d82ca4e3
MorrisHHS <23085975@student.hhs.nl>
2025-01-24 13:49:48 +0100

    Tests

---
+++ WPRRewriteTests/Tests/AccountTests/AccountControllerTests.cs
@@ -0,0 +1,67 @@
+﻿// Tests/AccountTests/AccountControllerTests.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WPRRewrite.Controllers;
+using WPRRewrite.Modellen.Accounts;
+
+namespace WPRRewriteTests.Tests.AccountTests
+{
+    [TestFixture]
+    public class AccountControllerTests
+    {
+        private Context _mockContext;
+        private AccountController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<Context>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _mockContext = new Context(options);
+            _controller = new AccountController(_mockContext);
+        }
+
+        [Test]
+        public async Task GetAll_GeenAccounts_RetourneertNotFound()
+        {
+            // Arrange: Geen accounts toevoegen aan de in-memory database
+
+            // Act
+            var result = await _controller.GetAccounts(null, null);
+
+            // Assert
+            Assert.That(result.Result, Is.TypeOf<NotFoundObjectResult>());
+        }
+
+        [Test]
+        public async Task GetAll_AccountsAanwezig_RetourneertOk()
+        {
+            // Arrange
+            _mockContext.Accounts.Add(new AccountParticulier("test@test.com", "test123", "Test Naam", 1234567890, 1));
+            await _mockContext.SaveChangesAsync();
+
+            // Act
+            var result = await _controller.GetAccounts(null, null);
+
+            // Assert
+            Assert.That(result.Result, Is.TypeOf<OkObjectResult>());
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            _mockContext.Dispose();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ WPRRewriteTests/Tests/AccountTests/AccountParticulierTests.cs
@@ -0,0 +1,37 @@
+﻿// Tests/AccountTests/AccountParticulierTests.cs
+using WPRRewrite.Dtos;
+using WPRRewrite.Enums;
+using WPRRewrite.Modellen.Accounts;
+
+namespace WPRRewriteTests.Tests.AccountTests
+{
+    [TestFixture]
+    public class AccountParticulierTests
+    {
+        [Test]
+        public void UpdateAccount_WijzigtGegevensCorrect()
+        {
+            // Arrange
+            var account = new AccountParticulier("test@test.nl", "wachtwoord123", "Test Gebruiker", 0612345678, 1);
+            var nieuweGegevens = new AccountDto(
+                AccountTypeEnum.Particulier,
+                "nieuw@test.nl",
+                "nieuwWachtwoord",
+                "Nieuwe Naam",
+                0687654321,
+                1
+            );
+
+            // Act
+            account.UpdateAccount(nieuweGegevens);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(account.Email, Is.EqualTo("nieuw@test.nl"));
+                Assert.That(account.Naam, Is.EqualTo("Nieuwe Naam"));
+                Assert.That(account.Telefoonnummer, Is.EqualTo(0687654321));
+            });
+        }
+    }
+}
